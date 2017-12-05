95a428d39b50c0adc634ef55055704f0f50398b6
Ben Stockett <thebenstockett@gmail.com>
2017-12-05 12:39:39 -0500

    Imported branch

--- exec.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-qemu-system-x86_64 -fda bin/PSOS.img -enable-kvm -rtc base=localtime -soundhw pcspk $@ 2> /dev/null
--- src/fs/fat16.asm
+++
@@ -1,5 +0,0 @@
-dw 0xF0FF, 0xFFFF
-times (FAT_SIZE * 512) - ($ - $$) db 0 ; FAT table
-
-times (ENTRIES * 32) db 0 ; root directory
-times (CLUSTERS * 512) db 0 ; data area
